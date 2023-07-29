8bcefaa84a259f1ae4f0608418ede1a5eb536d1a
Simon Litges <simon.litges@stud.hn.de>
2023-07-29 18:56:48 +0200

    Gauss-Eleimination neu implementiert

--- Numerik-Prüfungsvorbereitung/Uebungen/Kapitel_2/U4/gauss.cpp
+++ Numerik-Prüfungsvorbereitung/Uebungen/Kapitel_2/U4/gauss.cpp
@@ -1,278 +1,205 @@
+/**
+@file Prgramm, dass die Gauss-Elimination implementiert
+*/
+
 #include <iostream>
+#include <iomanip>
 #include <cmath>
-//#define test
 using namespace std;
 
 /**
- * Funktion zur Ausgabe einer quadratischen Matrix der Größe n x n
- * @param m Matrix (zweidimensionales Array), das ausgegeben werden soll
- * @param n Größe der Matrix n x n
- */
-void printMat(double** m, int n) {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << m[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-/**
- * Funktion zur Erzeugung einer neuen uninitialisierten Matrix der Größe n x n
- * @param n Größe der Matrix n x n
- * @return Zeiger auf die neue Matrix
- */
-double** newMat(int n) {
-    double **m = new double*[n];
+Funktion zur Erzeugung einer initialisierten quadratischen Matrix
+@param n Anzahl der Spaten/Zeilen der Matrix
+@param init Initialer Wert der Matrix (Default a = 1)
+@return Zeiger auf die erzeugte Matrix
+*/
+double** newMat(int n, double init = 1) {
+	//Speicher reservieren
+	double** m = new double*[n];
 	m[0] = new double[n*n];
-    for(int i = 1; i<n; i++) {
-        m[i] = *m+(i*n);
-    }
-    return m;
+	for(int i = 1; i<n; i++) {
+		m[i] = *m+(i*n);
+	}
+	
+	//Matrix initialisieren
+	for(int i = 0; i<n; i++) {
+		for(int j = 0; j<n; j++) {
+			m[i][j] = init;
+		}
+	}
+	
+	return m;
 }
 
 /**
- * Funktion zur Speicherfreigabe einer n x n Matrix
- * @param m Matrix, die Freigegeben werden soll
- * @param n Größe der Matrix n x n
- */
+Funktion zur Speicherfreigabe einer Matrix
+@param m Zeiger auf die freizugebende Matrix
+*/
 void delMat(double** m) {
-	delete m[0];
-    delete[] m;
+	delete[] m[0];
+	delete[] m;
 }
 
 /**
- * Funktion zur Ausgabe eines Vektors der Größe n
- * @param v Zeiger auf den Vektor
- * @param n Größe n des Vektors
- */
-void printVec(double* v, int n) {
-    for(int i = 0; i<n; i++) {
-        cout << v[i] << "\n";
-    }
-    cout << flush;
+Funktion zur Erzeugung eines initialisierten Vektors der Länge n
+@param n Anzahl der Elemente des Vektors
+@param init Initialer Wert der Vektorelemente
+@return Zeiger auf den erzeugten Vektor
+*/
+double* newVec(int n, double init = 1) {
+	//Speicher reservieren
+	double* v = new double[n];
+	
+	//Vektor initialisieren
+	for(int i = 0; i<n; i++) {
+		v[i] = init;
+	}
+	
+	return v;
 }
 
 /**
- * Funktion zur Erzeugung eines uninitialisierten Vektors der Größe n
- * @param n Größe n des Vektors
- * @return Zeiger auf den neuen Vektor
- */
-double* newVec(int n) {
-    return new double[n];
-}
-
-/**
- * Funktion zur Speicherfreigabe eines Vektors
- * @param v Vektor, der freigegeben werden soll
- */
+Funktion zur Speicherfreigabe eines Vektors
+@param v Zeiger auf den freizugebenden Vektor
+*/
 void delVec(double* v) {
-    delete[] v;
+	delete[] v;
 }
 
 /**
- * Funktion erzeugt eine neue quadratische Matrix der Größe n x n und initialisiert diese mit ganzzahligen Zufallswerten zwischen 0 und 4
- * @param n Größe n der Matrix
- * @return Zeiger auf die neue zufällige Matrix
- */
-double** randomMat(int n) {
-    double **m = newMat(n);
-
-    // Seed für die Zufallszahlengenerierung festlegen
-    srand(clock());
-    
-    //Zufallszahlen zwischen 0 und 4 einfügen
-    for(int i = 0; i<n; i++) {
-        for(int j = 0; j<n; j++) {
-            // Zufallszahl zwischen 0 und 4 generieren
-            m[i][j] = (double)(rand() % 5);
-        }
-    }
-    return m;
+Funktion, die eine Gauss-Elimination mit Spaltenpivotsuche durchführt. Übergebene Matrix und Vektor werden modifiziert.
+@param m Zeiger auf die Matrix, die in Zeilenstufenform umgeformt werden soll
+@param v Zeiger auf den Vektor, der die rechte Seite des Gleichungssystems bildet
+@param n Größe der Matrix/des Vektors in einer Dimension
+@return boolean, ob das Gleichungssystem eine Lösung hat
+*/
+bool gauss_elimination(double** m, double*v, int n) {
+	double** l = newMat(n);
+	for(int i = 0; i < n-1; i++) {
+		
+		//Spaltenpivotsuche für aktuelle Spaltenpivotsuche
+		
+		//betragsmäßig größtes Element in aktueller Spalte suchen
+		int maxzeile = i;
+		for(int k = i+1; k<n; k++) {
+			if(fabs(m[k][i]) > fabs(m[maxzeile][i])) {
+				maxzeile = k;
+			}
+		}
+		
+		//Falls größtes Element 0: Determinante der Matrix ist 0, also hat Gleichungssystem keine Lösung
+		if(fabs(m[maxzeile][i]) == 0) {
+			return false;
+		}
+		
+		//Zeile des betragsmäßig größten Elements mit der aktuellen Zeile des Diagonalelements vertauschen
+		if(maxzeile != i) {
+			//Zeilen Matrix tauschen
+			double* tmp = m[maxzeile];
+			m[maxzeile] = m[i];
+			m[i] = tmp;
+			
+			//Elemente Vektor tauschen
+			double tmp2 = v[maxzeile];
+			v[maxzeile] = v[i];
+			v[i] = tmp2;
+		}
+		
+		//Gauss-Elimination durchführen
+		
+		
+		//Zeilen unter dem Diagonalelement der aktuellen Spalte durchgehen und Faktoren berechnen
+		for(int j = i+1; j < n; j++) {
+			l[j][i] = m[j][i]/m[i][i];
+		}
+		
+		//Zeilen unter dem Diagonalelement der aktuellen Spalte durchgehen und Elemente zu 0 machen
+		for(int j = i+1; j < n; j++) {
+			//Elemente der Zeile durchgehen
+			for(int k = i; k < n; k++) {
+				m[j][k] = m[j][k] - (l[j][i] * m[i][k]);
+			}
+			
+			//Vektorelemente bearbeiten
+			v[j] = v[j] - (l[j][i] * v[i]);
+		}
+		
+	}
+	delMat(l);
+	return true;
 }
 
 /**
- * Funktion erzeugt einen neuen Vektor der Größe n und initialisiert diesen mit ganzzahligen Zufallswerten zwischen 0 und 4
- * @param n Größe n des Vektors
- * @return Zeiger auf den neuen zufälligen Vektor
- */
-double* randomVec(int n) {
-    double *v = newVec(n);
-
-    // Seed für die Zufallszahlengenerierung festlegen
-    srand(clock());
-    
-    //Zufallszahlen zwischen 0 und 4 einfügen
-    for(int i = 0; i<n; i++) {
-        // Zufallszahl zwischen 0 und 4 generieren
-        v[i] = (double)(rand() % 5);
-    }
-    return v;
+Funktion, die ein GLeichungssystem löst und den Ergebnisvektor berechnet.
+@param m Matrix in Zeilenstufenform
+@param v Vektor der rechten Seite des Gleichungssystems
+@param n Größe der Matrix/des Vektors in einer Dimension
+@return Zeiger auf den Ergebnisvektor
+*/
+double* solve(double** m, double* v, int n) {
+	double* x = newVec(n);
+	double sum;
+	
+	//x[n] berechnen
+	x[n-1] = v[n-1]/m[n-1][n-1];
+	
+	for(int k = n-2; k >= 0; k--) {
+		
+		//Summe der bekannten Elemente berechnen
+		sum = 0.0;
+		for(int i = k+1; i < n; i++) {
+			sum += (m[k][i] * x[i]);
+		}
+		x[k] = (v[k] - sum)/m[k][k];
+	}
+	
+	return x;
 }
 
 /**
- * Funktion führt Gauss-Elimination durch
- * @param a Matrix, die in Zeilenstufenform umgeformt wird
- * @param b Vektor, der zur Matrix gehört und verändert wird
- * @param n Größe der n x n Matrix bzw. n des Vektors
- * @return Bool, ob der die Matrix eine Lösung hat (false, falls Matrix singulär)
- */
-bool gauss(double** a, double* b, int n) {
-    double** l = newMat(n); //Speichert die Faktoren der Gauss-Elimination
-
-    //Für jede Zeile k ausser der letzten
-    for(int k = 0; k<n-1; k++) {
-
-        //Spaltenpivotsuche
-        int maxzeile = k;
-
-        //Maximum bestimmen
-        for(int m = k+1; m<n; m++) {
-            if(fabs(a[m][k]) > fabs(a[maxzeile][k])) {
-                maxzeile = m;
-            }
-        }
-
-        //Falls Null auf Diagonalen: Matrix ist Singulär und hat keine Lösung
-        if(fabs(a[maxzeile][k]) == 0) {
-            return false;
-        }
-
-        //Vertausche Zeilen k und maxzeile falls k != maxzeile
-        if(maxzeile != k) {
-            //Zeilen vertauschen
-            double *tmp = a[k];
-            a[k] = a[maxzeile];
-            a[maxzeile] = tmp;
-            
-            //Vektorelemente vertauschen
-            double tmp2 = b[k];
-            b[k] = b[maxzeile];
-            b[maxzeile] = tmp2;
-        }
-
-        //unten liegende Zeilen i durchgehen und Faktoren berechnen
-        for(int i = k+1; i<n; i++) {
-            l[i][k] = a[i][k]/a[k][k];
-        }
-
-        //unten liegende Zeilen i durchgehen und neue Werte berechnen
-        for(int i = k+1; i<n; i++) {
-            //rechts liegende Spalten j von i durchgehen
-            for(int j = k; j<n; j++) {
-                a[i][j] = a[i][j] - l[i][k]*a[k][j];
-            }
-            //Rechte Seite ändern
-            b[i] = b[i] - l[i][k]*b[k];
-        }
-    }
-    return true;
+Funktion, die das Gleichungssystem Ax = b mittels Gauss-Elimination löst und den Ergebnisvektor berechnet.Übergebene Matrix und Vektor werden modifiziert.
+@param m Zeiger auf die Matrix des Gleichungssystems
+@param v Zeiger auf den Vektor des Gleichungssystems
+@param n Größe der Matrix/des Vektors in einer Dimension
+@return Zeiger auf den Ergebnisvektor, nullptr falls LGS keine Lösung hat
+*/
+double* gauss(double**m, double* v, int n) {
+	if(!gauss_elimination(m,v,n)) {
+		return nullptr;
+	}
+	return solve(m,v,n);
 }
 
-/**
- * Funktion zur Lösung eines Gleichungssystems einer Matrix in Zeilenstufenform
- * @param a Matrix in Zeilenstufenform
- * @param b Vektor zur Matrix in Zeilenstufenform
- * @param n Größe der Matrix n x n und Größe des Vektors n
- * @return Zeiger auf den Ergebnisvektor des Gleichungssystems
- */
-double* solve(double **a, double* b, int n) {
-    double *result = newVec(n);
-
-    //Matrix liegt in Zeilenstufenform vor
-
-    //erstes Ergebnis berechnen
-    result[n-1] = b[n-1]/a[n-1][n-1];
-
-    //andere Ergebnisse berechnen
-    double absWert;
-    for(int i = n-2; i>=0; i--) {
-        //absWert berechnen
-        absWert = 0;
-        for(int j = i+1; j<n; j++) {
-            absWert += a[i][j]*result[j];
-        }
-
-        //Unbekannte Ausrechnen
-        result[i] = (b[i]-absWert)/a[i][i];
-    }
-    return result;
-}
-
-/**
- * Funktion führt Gauss-Elimination durch und berechnet den Ergebnisvektor
- * @param mat quadratische Matrix der Größe n x n
- * @param vec Vektor der Größe n
- * @param n Größe der Matrix/des Vektors
- * @return Zeiger auf den Ergebnisvektor. Falls es keine (eindeutige) Lösung gibt: nullptr
- */
-double* gaussSolve(double** mat, double* vec, int n) {
-    if(!gauss(mat,vec,n)) {
-        return nullptr;
-    }
-    return solve(mat, vec, n);
-}
-
-/**
- * Main-Funktion zum Test des Programms
- * @return Return-Code des Programms
- */
 int main() {
-    int n = 4;
-
-#ifdef test
-    double **mat = newMat(n);
-    double *vec = newVec(n);
-
-    //Initialisierung der Matrix
-    mat[0][0] = 1;
-    mat[0][1] = 3;
-    mat[0][2] = 2;
-    mat[0][3] = 9;
-
-    mat[1][0] = 2;
-    mat[1][1] = 6;
-    mat[1][2] = 4;
-    mat[1][3] = 18;
-
-    mat[2][0] = 2;
-    mat[2][1] = 6;
-    mat[2][2] = 4;
-    mat[2][3] = 3;
-
-    mat[3][0] = 4;
-    mat[3][1] = 3;
-    mat[3][2] = 2;
-    mat[3][3] = 1;
-
-    vec[0] = 1;
-    vec[1] = 2;
-    vec[2] = 3;
-    vec[3] = 4;
-#else
-    double **mat = randomMat(n);
-    double *vec = randomVec(n);
-#endif
-    cout << "Ursprungsmatrix\n";
-    printMat(mat, n);
-    cout << "\nUrsprungsvektor\n";
-    printVec(vec,n);
-
-    if(!gauss(mat,vec,n)) {
-        cout << "\nMatrix ist singulär!" << endl;
-        return -1;
-    }
-
-    cout << "\nMatrix in Zeilenstufenform\n";
-    printMat(mat,n);
-    cout << "\nVektor zur Matrix in Zeilenstufenform\n";
-    printVec(vec,n);
-
-    //Ergebnis ausgeben
-    cout << "\nErgebnisvektor\n";
-    printVec(solve(mat,vec,n),n);
-
-    delMat(mat);
-    delVec(vec);
-    return 0;
+	double** m = newMat(3);
+	double* v = newVec(3);
+	
+	//Testdaten in Matrix und Vektor schreiben
+	m[0][0] = 1;
+	m[0][1] = 2;
+	m[0][2] = -1;
+	
+	m[1][0] = 1;
+	m[1][1] = 1;
+	m[1][2] = 2;
+	
+	m[2][0] = 2; 
+	m[2][1] = 3;
+	m[2][2] = -3;
+	
+	v[0] = 2;
+	v[1] = 9;
+	v[2] = -1;
+	
+	//Gauss-Elimination durchführen
+	double* x = gauss(m,v,3);
+	
+	//Ausgabe Ergebnisvektor
+	for(int i = 0; i < 3; i++) {
+		cout << x[i] << endl;
+	}
+	
+	delMat(m);
+	delVec(v);
 }
+	
